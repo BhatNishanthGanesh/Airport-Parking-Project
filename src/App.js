cbc861b9e5b799f85426c0546c950fb2ae9514ab
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-03-03 18:51:06 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Name/> */}
+    <div className="App"> 
+    {/* <Name/> */}
      <Homepage/>
     </div>
   );
