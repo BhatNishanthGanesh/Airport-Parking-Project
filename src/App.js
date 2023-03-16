43ed9a60134eded0ef7c49a149598eba1baea57a
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-03-16 17:12:36 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Homepage from './Pages/Home';
 import './style.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import AirportAvailability from './Pages/AirportAvailability';
-import Layout from './component/Layout';
+import Layout from './components/Layout';
 import Login from './Pages/Login';
 function App() {
   return (
